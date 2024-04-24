fee6f9d3d619def7cf8dfcff9941d7b28d55f861
Denison Kolling <denisonkolling@gmail.com>
2024-04-24 09:51:09 -0300

    first commit

---
+++ src/main/java/com/example/reactroutes/ReactRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactRoutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactroutes/ReactRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
